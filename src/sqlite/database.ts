8cda221cca173f44536c5c32897ece49e56a76a4
Ruhannn <ruhanrouf17@gmail.com>
2024-12-14 03:29:11 +0600

    fixed db file sqlite for sqlite

--- src/sqlite/database.ts
+++ src/sqlite/database.ts
@@ -1,6 +1,6 @@
 import {verbose} from "sqlite3";
 const sqlite3 = verbose();
-export const db = new sqlite3.Database('./hm.db', (err) => {
+export const db = new sqlite3.Database('./src/sqlite/hm.db', (err) => {
     if (err) {
         console.error(err.message);
     } else {
